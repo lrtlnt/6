03e50024b859520ebecb084cd3010377937e4289
lrtlnt <beijingxubao@163.com>
2026-07-31 10:07:02 +0800

    Add script to fetch SDL and its libraries

---
+++ fetch_sdl.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+mkdir -p android/app/src/main/thirdparty
+cd android/app/src/main/thirdparty
+git clone https://github.com/libsdl-org/SDL.git
+git clone https://github.com/libsdl-org/SDL_image.git
+git clone https://github.com/libsdl-org/SDL_ttf.git
